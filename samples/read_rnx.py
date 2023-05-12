284fc84cc49c5fa14cad3f69848ad896d61f4833
Hauschild, André <andre.hauschild@dlr.de>
2023-05-12 11:07:49 +0200

    Adaption to new RINEX module

--- samples/read_rnx.py
+++ samples/read_rnx.py
@@ -1,5 +1,6 @@
 import numpy as np
 from cssrlib.gnss import Nav, ecef2pos, geodist, satazel, timediff, uGNSS
+from cssrlib.gnss import rSigRnx
 from cssrlib.ephemeris import findeph, eph2pos
 from cssrlib.plot import skyplot, plot_elv
 from cssrlib.rinex import rnxdec
@@ -7,7 +8,21 @@ from cssrlib.rinex import rnxdec
 navfile = '../data/SEPT078M.21P'
 obsfile = '../data/SEPT078M.21O'
 
+# Define signals to be processed
+#
+sigs = [rSigRnx("GC1C"), rSigRnx("GC2W"),
+        rSigRnx("GL1C"), rSigRnx("GL2W"),
+        rSigRnx("GS1C"), rSigRnx("GS2W"),
+        rSigRnx("EC1C"), rSigRnx("EC5Q"),
+        rSigRnx("EL1C"), rSigRnx("EL5Q"),
+        rSigRnx("ES1C"), rSigRnx("ES5Q"),
+        rSigRnx("JC1C"), rSigRnx("JC5Q"),
+        rSigRnx("JL1C"), rSigRnx("JL5Q"),
+        rSigRnx("JS1C"), rSigRnx("JS5Q")]
+
+
 dec = rnxdec()
+dec.setSignals(sigs)
 nav = Nav()
 nav = dec.decode_nav(navfile, nav)
 
