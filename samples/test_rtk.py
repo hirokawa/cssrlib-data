f7483367357e9504a927b849c3b0eb3ced14b931
Rui Hirokawa <rui.hirokawa@gmail.com>
2023-08-29 22:20:59 +0900

    - corrected reference position. - added obs data for doy238a

--- samples/test_rtk.py
+++ samples/test_rtk.py
@@ -15,11 +15,21 @@ from cssrlib.rtk import rtkinit, relpos
 
 bdir = '../data/'
 atxfile = bdir+'igs14.atx'
-navfile = bdir+'SEPT078M.21P'
-obsfile = bdir+'SEPT078M.21O'
-basefile = bdir+'3034078M.21O'
+ngsantfile = bdir+'GSI_PCV.TXT'
 
-xyz_ref = [-3962108.673,   3381309.574,   3668678.638]
+if False:
+    navfile = bdir+'SEPT078M.21P'
+    obsfile = bdir+'SEPT078M.21O'
+    basefile = bdir+'3034078M.21O'
+    xyz_ref = [-3962108.673, 3381309.574, 3668678.638]
+    nav.rb = [-3959400.631, 3385704.533, 3667523.111]  # GSI 3034 fujisawa
+else:
+    navfile = bdir+'SEPT238A.23P'
+    obsfile = bdir+'SEPT238A.23O'
+    basefile = bdir+'3034238A.23O'
+    xyz_ref = [-3962108.7007, 3381309.5532, 3668678.6648]
+    nav.rb = [-3959400.6443, 3385704.4948, 3667523.1275]  # GSI 3034 fujisawa
+
 pos_ref = gn.ecef2pos(xyz_ref)
 
 # Define signals to be processed
@@ -58,13 +68,13 @@ decb.setSignals(sigsb)
 decb.decode_obsh(basefile)
 dec.decode_obsh(obsfile)
 
-nep = 180
+nep = 300
 
-# GSI 3034 fujisawa
-nav.rb = [-3959400.631, 3385704.533, 3667523.111]
+
 t = np.zeros(nep)
 enu = np.zeros((nep, 3))
 smode = np.zeros(nep, dtype=int)
+# ecef_ = np.zeros((nep, 3))
 
 rtkinit(nav, dec.pos)
 rr = dec.pos
@@ -73,6 +83,7 @@ rr = dec.pos
 #
 atx = atxdec()
 atx.readpcv(atxfile)
+atx.readngspcv(ngsantfile)
 
 # Set PCO/PCV information
 #
@@ -104,10 +115,13 @@ for ne in range(nep):
     t[ne] = gn.timediff(nav.t, t0)
     sol = nav.xa[0:3] if nav.smode == 4 else nav.x[0:3]
     enu[ne, :] = gn.ecef2enu(pos_ref, sol-xyz_ref)
+    # ecef_[ne, :] = sol
     smode[ne] = nav.smode
 
 dec.fobs.close()
 decb.fobs.close()
+
+# xyz_ref = np.mean(ecef_, axis=0)
 
 fig_type = 1
 ylim = 0.2
